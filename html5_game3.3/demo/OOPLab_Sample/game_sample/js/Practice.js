1b6a23a542d3c22e0ef9b86fafcc0d061a7bd16e
Johnny <milkkuan@gmail.com>
2021-03-19 10:45:49 +0800

    html5_practice

---
+++ html5_game3.3/demo/OOPLab_Sample/game_sample/js/Practice.js
@@ -0,0 +1,33 @@
+var Practice = function(){
+    this.load = function(){
+        this.pic = new Framework.Sprite(define.imagePath + 'firststage.png');
+        this.pic.position = {
+            x: 800,
+            y: 450
+        }
+        this.pic.rotation = 0;
+        this.position = {
+            x: 800,
+            y: 450
+        }
+        this.rotation = 0;
+    };
+
+    this.initialize = function(){
+
+    };
+
+    this.update = function(){
+        this.position = {
+            x: this.position.x + 1,
+            y: this.position.y
+        }
+        this.rotation +=1;
+        this.pic.position = this.position
+        this.pic.rotation = this.rotation
+    };
+
+    this.draw = function(ctx){
+        this.pic.draw(ctx)
+    };
+};
