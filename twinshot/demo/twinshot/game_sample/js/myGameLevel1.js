505e06a7ae068864533a6f6cb1e351d0d5a56db1
上官昌華 <hellen.sk0115@gmail.com>
2021-03-26 11:16:16 +0800

    delete clock

--- twinshot/demo/twinshot/game_sample/js/myGameLevel1.js
+++ twinshot/demo/twinshot/game_sample/js/myGameLevel1.js
@@ -14,52 +14,52 @@
         //     y : 450
         // }
 
-	    var characterPosition;
+	    // var characterPosition;
 
         // this.rotation = 0;
         // this.rootScene.attach(this.pic);
         // this.pic.draw();
 
-        this.isStop = false;
-        this.isPlayed = false;
+        // this.isStop = false;
+        // this.isPlayed = false;
 
-        this.clock = new Framework.Sprite(define.imagePath + 'clock.png');
-        this.clock.scale = 0.3;
-        this.clock.position = {
-            x: 0,
-            y: 0
-        };
+        // this.clock = new Framework.Sprite(define.imagePath + 'clock.png');
+        // this.clock.scale = 0.3;
+        // this.clock.position = {
+        //     x: 0,
+        //     y: 0
+        // };
 
-        characterPosition = {x: 0, y: -1138 * this.clock.scale};
-        this.secondHand = new Framework.Sprite(define.imagePath + 'secondHand.jpg'); 
-        this.firen = new Character(define.imagePath + 'firen.png', {position: characterPosition, run: {from: 20, to: 22}, beHit: {from:30, to: 35}, hit: {from: 10, to: 13}}); 
-        this.freeze = new Character(define.imagePath + 'freeze.png', {position: characterPosition, scale: 1, run: {from: 29, to: 27}, beHit: {from:39, to: 35}, hit: {from: 19, to: 16}});
+        // characterPosition = {x: 0, y: -1138 * this.clock.scale};
+        // this.secondHand = new Framework.Sprite(define.imagePath + 'secondHand.jpg'); 
+        // this.firen = new Character(define.imagePath + 'firen.png', {position: characterPosition, run: {from: 20, to: 22}, beHit: {from:30, to: 35}, hit: {from: 10, to: 13}}); 
+        // this.freeze = new Character(define.imagePath + 'freeze.png', {position: characterPosition, scale: 1, run: {from: 29, to: 27}, beHit: {from:39, to: 35}, hit: {from: 19, to: 16}});
 
-        this.clockCenter = new Framework.Scene();
-        this.clockCenter.position = {
-            x: -10.5 * this.clock.scale,
-            y: 51 * this.clock.scale
-        };
+        // this.clockCenter = new Framework.Scene();
+        // this.clockCenter.position = {
+        //     x: -10.5 * this.clock.scale,
+        //     y: 51 * this.clock.scale
+        // };
 
-        this.clockCenterNeg = new Framework.Scene();
-        this.clockCenterNeg.position = {
-            x: -10.5 * this.clock.scale,
-            y: 51 * this.clock.scale
-        };
+        // this.clockCenterNeg = new Framework.Scene();
+        // this.clockCenterNeg.position = {
+        //     x: -10.5 * this.clock.scale,
+        //     y: 51 * this.clock.scale
+        // };
 
-        this.secondHand.position = {
-            x: 0,
-            y: -100
-        };
+        // this.secondHand.position = {
+        //     x: 0,
+        //     y: -100
+        // };
 
-        this.wholeClock = new Framework.Scene();
-        this.wholeClock.position = {
-            x: Framework.Game.getCanvasWidth() / 2,
-            y: Framework.Game.getCanvasHeight() / 2
-        };
+        // this.wholeClock = new Framework.Scene();
+        // this.wholeClock.position = {
+        //     x: Framework.Game.getCanvasWidth() / 2,
+        //     y: Framework.Game.getCanvasHeight() / 2
+        // };
 
 
-        this.secondHandRotationRate = 0.3;
+        // this.secondHandRotationRate = 0.3;
         // this.wholeClock.attach(this.clock);
         // this.clockCenter.attach(this.secondHand);
         // this.clockCenter.attach(this.firen.sprite);
@@ -69,8 +69,8 @@
         // this.rootScene.attach(this.wholeClock);
 
         //繪製Sprite的boundry (Debug用)
-        this.firen.sprite.isDrawBoundry = true;
-        this.clock.isDrawBoundry = true;
+        // this.firen.sprite.isDrawBoundry = true;
+        // this.clock.isDrawBoundry = true;
 
         //載入要被播放的音樂清單
         //資料夾內只提供mp3檔案, 其餘的音樂檔案, 請自行轉檔測試
@@ -122,34 +122,34 @@
         // this.pic.rotation = this.rotation;
 		
         //以下為當被攻擊時會停下來, 並且當被攻擊的動畫播放完時便繼續跑的Scenario
-        if(this.firen.collide(this.freeze) && !this.isStop && !this.isPlayed) {
-            this.isStop = true;
-            this.isPlayed = true;
-            //當碰攻擊時, 播放音效(可一次播放多首音樂)
-            this.audio.play({name: 'kick'});
-            this.firen.hit(function() {
-                game.freeze.beHit(function() {
-                    game.isStop = false;
-                    game.freeze.run();
-                });
-                game.firen.run();
-            });
+        // if(this.firen.collide(this.freeze) && !this.isStop && !this.isPlayed) {
+        //     this.isStop = true;
+        //     this.isPlayed = true;
+        //     //當碰攻擊時, 播放音效(可一次播放多首音樂)
+        //     this.audio.play({name: 'kick'});
+        //     this.firen.hit(function() {
+        //         game.freeze.beHit(function() {
+        //             game.isStop = false;
+        //             game.freeze.run();
+        //         });
+        //         game.firen.run();
+        //     });
             
-        }
-        else if(!this.firen.collide(this.freeze)){
-            this.isPlayed = false;
-            this.clockCenter.rotation += this.secondHandRotationRate;
-            this.clockCenterNeg.rotation = -this.clockCenter.rotation;
-        }
-        else if(this.firen.collide(this.freeze) && !this.isStop)
-        {
-            this.clockCenter.rotation += this.secondHandRotationRate;
-            this.clockCenterNeg.rotation = -this.clockCenter.rotation;
-        }
+        // }
+        // else if(!this.firen.collide(this.freeze)){
+        //     this.isPlayed = false;
+        //     this.clockCenter.rotation += this.secondHandRotationRate;
+        //     this.clockCenterNeg.rotation = -this.clockCenter.rotation;
+        // }
+        // else if(this.firen.collide(this.freeze) && !this.isStop)
+        // {
+        //     this.clockCenter.rotation += this.secondHandRotationRate;
+        //     this.clockCenterNeg.rotation = -this.clockCenter.rotation;
+        // }
         //以上為當被攻擊時會停下來, 並且當被撞到的動畫播放完時便繼續跑的Scenario
 
 
-        this.isPlayHit = this.firen.collide(this.freeze)                               
+        // this.isPlayHit = this.firen.collide(this.freeze)                               
     },
 
     draw:function(parentCtx){
@@ -171,24 +171,24 @@
 
         this.practice.keydown(e, list);
 
-        Framework.DebugInfo.Log.warning(e.key);
-        if(e.key === 'Numpad +' || e.key === '=') {
-            this.secondHandRotationRate += 0.05;
-        }
+        // Framework.DebugInfo.Log.warning(e.key);
+        // if(e.key === 'Numpad +' || e.key === '=') {
+        //     this.secondHandRotationRate += 0.05;
+        // }
 
-        if(e.key === 'Numpad -' || e.key === '-') {
-            this.secondHandRotationRate -= 0.05;
-        }
+        // if(e.key === 'Numpad -' || e.key === '-') {
+        //     this.secondHandRotationRate -= 0.05;
+        // }
 
-        if(e.key === 'Pause/Break') {
-            //AnimationSprite支援停止正在播放的圖片
-            this.firen.sprite.stop();
-        }
+        // if(e.key === 'Pause/Break') {
+        //     //AnimationSprite支援停止正在播放的圖片
+        //     this.firen.sprite.stop();
+        // }
 
-        if(e.key === 'F5') {
-            //AnimationSprite可以恢復暫停正在播放的圖片
-            this.firen.sprite.resume();
-        }
+        // if(e.key === 'F5') {
+        //     //AnimationSprite可以恢復暫停正在播放的圖片
+        //     this.firen.sprite.resume();
+        // }
 
         if(e.key === 'Enter') {
             if(!this.isFullScreen) {
@@ -215,24 +215,24 @@
             return;
         }  
         
-        if(e.x >= this.rectPosition.x && e.x <= this.rectPosition.x + 260 && e.y >= this.rectPosition.y && e.y <= this.rectPosition.y + 90) {
-            if(!this.isClockStop) {
-                this.secondHandRotationRate = 0;
-                this.isClockStop = true;
-                //Audio可以一次暫停所有的音樂
-                this.audio.pauseAll();
-            } else {
-                this.isClockStop = false;
-                this.secondHandRotationRate = 0.3;
-                //Audio也可以針對一首歌進行操作(繼續播放)
-                this.audio.resume('song2');
-            }
-        } else if(e.x >= this.clock.upperLeft.x && e.x <= this.clock.lowerRight.x && e.y >= this.clock.upperLeft.y && e.y <= this.clock.lowerRight.y) {
-            //由於Click Me在太小的螢幕的情況下會蓋到Clock, 導致點擊Click Me時, 會回到前一個Level,
-            //故使用else if, 並優先選擇Click Me會觸發的條件
-            this.audio.stopAll();
-            Framework.Game.goToPreviousLevel();            
-            return;
-        }
+        // if(e.x >= this.rectPosition.x && e.x <= this.rectPosition.x + 260 && e.y >= this.rectPosition.y && e.y <= this.rectPosition.y + 90) {
+        //     if(!this.isClockStop) {
+        //         this.secondHandRotationRate = 0;
+        //         this.isClockStop = true;
+        //         //Audio可以一次暫停所有的音樂
+        //         this.audio.pauseAll();
+        //     } else {
+        //         this.isClockStop = false;
+        //         this.secondHandRotationRate = 0.3;
+        //         //Audio也可以針對一首歌進行操作(繼續播放)
+        //         this.audio.resume('song2');
+        //     }
+        // } else if(e.x >= this.clock.upperLeft.x && e.x <= this.clock.lowerRight.x && e.y >= this.clock.upperLeft.y && e.y <= this.clock.lowerRight.y) {
+        //     //由於Click Me在太小的螢幕的情況下會蓋到Clock, 導致點擊Click Me時, 會回到前一個Level,
+        //     //故使用else if, 並優先選擇Click Me會觸發的條件
+        //     this.audio.stopAll();
+        //     Framework.Game.goToPreviousLevel();            
+        //     return;
+        // }
     },
 });
